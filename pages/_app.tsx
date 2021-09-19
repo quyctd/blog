76630877050e0cb813a1eb03afb4c19e7ce14a5d
quyctd <quy.dc98@gmail.com>
2021-09-19 22:13:14 +0700

    :lipstick: Use light theme for now

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
-import '@styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from 'next-themes'
+
+import '@styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider disableTransitionOnChange defaultTheme="light">
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 export default MyApp
