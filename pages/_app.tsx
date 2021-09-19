4bbd0f61d03baa0661df36b258c9229d759da553
quyctd <quy.dc98@gmail.com>
2021-09-19 22:17:36 +0700

    :truck: Use nprogress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,17 @@
 import type { AppProps } from 'next/app'
+import Router from 'next/router'
 import { ThemeProvider } from 'next-themes'
 
+import nprogress from 'nprogress'
+
 import '@styles/globals.css'
+
+Router.events.on('routeChangeStart', () => nprogress.start())
+Router.events.on('routeChangeComplete', () => {
+  nprogress.done()
+  window.scrollTo(0, 0)
+})
+Router.events.on('routeChangeError', () => nprogress.done())
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
