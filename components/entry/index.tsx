9ad46083e65d2c06af8864c73aed694f0f7b9ee7
quyctd <quy.dc98@gmail.com>
2021-09-26 22:12:26 +0700

    :sparkles: Add design page

---
+++ components/entry/index.tsx
@@ -0,0 +1,53 @@
+import cn from 'classnames'
+import { useInView } from 'react-intersection-observer'
+import 'intersection-observer'
+
+import styles from './entry.module.css'
+const imagePrefix = `https://res.cloudinary.com/flask-image/image/fetch/q_60,w_1000`
+
+interface Props {
+  title: string
+  description: string
+  image: string
+  href: string
+  position?: string
+}
+
+const Entry: React.FC<Props> = ({
+  title,
+  description,
+  image,
+  href,
+  position,
+}) => {
+  const [ref, inView] = useInView({ triggerOnce: true })
+
+  return (
+    <a
+      href={href}
+      target="_blank"
+      rel="noopener noreferrer"
+      ref={ref}
+      className={cn(styles.link, { [styles.active]: !image })}
+      title={`${title} - ${description}`}
+    >
+      <section
+        style={{
+          backgroundImage: image
+            ? !inView
+              ? 'none'
+              : `url('${imagePrefix}/${encodeURIComponent(image)}')`
+            : 'none',
+          backgroundPosition: position ? position : 'center',
+        }}
+      >
+        <div>
+          <p className={cn(styles.title, 'clamp')}>{title}</p>
+          <p className={cn(styles.description, 'clamp')}>{description}</p>
+        </div>
+      </section>
+    </a>
+  )
+}
+
+export default Entry
