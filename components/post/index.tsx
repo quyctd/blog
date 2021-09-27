bfe576e13701db21ffa56f0af5054c138c35b4af
quyctd <quy.dc98@gmail.com>
2021-09-27 23:58:39 +0700

    :bug: Barely render blog detail for now

---
+++ components/post/index.tsx
@@ -0,0 +1,56 @@
+import Head from 'next/head'
+
+import Navigation from './navigation'
+import Page from '@components/page'
+import styles from './post.module.css'
+
+function escapeHtml(unsafe: string) {
+  return unsafe
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#039;')
+}
+
+const Post = ({
+  title,
+  slug,
+  html,
+  hidden,
+  og,
+  description,
+  date,
+  previous,
+  next,
+}: any) => {
+  return (
+    <Page
+      title={title}
+      description={description}
+      showHeaderTitle={false}
+      image={
+        og && og === true
+          ? `https://res.flask-image.com/dsdlhtnpw/image/upload/${slug}.png`
+          : og
+      }
+    >
+      <Head>
+        {hidden && <meta name="robots" content="noindex" />}
+        {date && <meta name="date" content={date} />}
+      </Head>
+
+      <article
+        dangerouslySetInnerHTML={{
+          __html: `<span class="${styles.date}">${date}</span><h1 class="${
+            styles.title
+          }">${escapeHtml(title)}</h1>${html}`,
+        }}
+      />
+
+      <Navigation previous={previous} next={next} />
+    </Page>
+  )
+}
+
+export default Post
