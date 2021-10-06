18ad8a3aab72cf88f4be04a82773572b31e9c0b2
quyctd <quy.dc98@gmail.com>
2021-10-06 23:54:11 +0700

    :sparkles: Init music page

---
+++ pages/music.tsx
@@ -0,0 +1,11 @@
+import Page from '@components/page'
+
+const Music = () => {
+  return (
+    <Page title="Music" description="Collection of the songs I love.">
+      <article>Coming soon...</article>
+    </Page>
+  )
+}
+
+export default Music
