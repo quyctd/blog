11a39fff183e0d1af32ad0391bfbc10b193057e7
quyctd <quy.dc98@gmail.com>
2021-10-06 23:55:06 +0700

    :sparkles: Init projects page

---
+++ pages/project.tsx
@@ -0,0 +1,11 @@
+import Page from '@components/page'
+
+const Projects = () => {
+  return (
+    <Page title="Projects" description="Collection of the projects I do.">
+      <article>Coming soon...</article>
+    </Page>
+  )
+}
+
+export default Projects
