02f46724228748ba98e9048b99d5129e5e41c801
quyctd <quy.dc98@gmail.com>
2021-12-22 22:33:27 +0700

    :speech_balloon: Update text

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,23 +7,15 @@ const About = () => {
       <article>
         <h1>Quy Dinh</h1>
         <p>
-          A newbie Front-End engineer, b. 1998, live in Hanoi, Vietnam. Self
-          alias as{' '}
+          A newbie Front-End engineer, b. 1998, live in Hanoi, Vietnam. Alias as{' '}
           <Link underline href="https://github.com/quyctd" external>
             @quyctd
           </Link>{' '}
           on the internet.
         </p>
         <p>
-          Interested in UI and UX, system design, practicing minimalist.{' '}
-          <Link underline href="/reading">
-            Reading
-          </Link>
-          {', '}
-          <Link underline href="/blog">
-            writing
-          </Link>{' '}
-          about code. Enjoy listening to music, watching anime.
+          Interested in UI and UX, system design, practicing minimalist. Focus on <Link underline href="/reading">
+          reading</Link>, thinking and learning. Believe being good means being free.
         </p>
       </article>
     </Page>
