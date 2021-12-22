6865765d59c64e201e301248d645d409aeba919f
quyctd <quy.dc98@gmail.com>
2021-12-22 22:51:28 +0700

    :speech_balloon: Update contact

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,17 +1,11 @@
 import Page from '@components/page'
+import Link from '@components/link'
 
 const Contact = () => {
   return (
     <Page title="Contact" description="Get in touch.">
-      <article>
-        <p>Get in touch.</p>
-
-        <blockquote>
-          <a href="mailto:quy.dc98@gmail.com?subject=Hello" className="reset">
-            quy.dc98@gmail.com
-          </a>
-        </blockquote>
-      </article>
+      <p>You can reach me at <Link underline href="https://www.facebook.com/quyctd/">@quyctd</Link> or <Link underline href="mailto:quy.dc98@gmail.com">quy.dc98@gmail.com</Link>.</p>
+      <p>I'm always open to interesting conversations and collaboration. If you're in Hanoi city, let's have some coffee.</p>
     </Page>
   )
 }
